a20e5983d84574d423f86cea9d80c0d6272def84
Chen Yue Zhang <CZhang13@slb.com>
2021-03-02 15:55:09 +0800

    feat(app): upgrade to sauth v2 token

--- providers/token.provider.ts
+++ providers/token.provider.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import NodeCache from 'node-cache';
+import querystring from 'querystring';
 
 import logger from '../common/logger';
 import consul from '../core/consul';
@@ -15,24 +16,29 @@ export async function getTokenAsync() {
     if (t) {
       return Promise.resolve(t);
     }
-    const [tokenUrl, apiKey, projectid, serviceid, secret] = await Promise.all([
-      consul('SAuth-ServiceToken-Uri'),
-      keyvault('SAuth-ServiceToken-ApiKey'),
-      consul('SAuth-ProjectId-Helios'),
-      consul('SAuth-ServiceId-Helios-ProvisionAgent'),
-      keyvault('SAuth-Secret-Helios-ProvisionAgent'),
-    ]);
+
+    const tokenServiceUrl = (await consul('SAuth-V2-Token-Service-Url')).replace(/\/$/, '');
+    const sauthClientId = await consul('SAuth-ServiceId-Helios-ProvisionAgent');
+    const sauthClientSecret = await keyvault('SAuth-Secret-Helios-ProvisionAgent');
+
+    const base64 = Buffer.from(`${sauthClientId}:${sauthClientSecret}`).toString('base64');
+    const basicAuthToken = `Basic ${base64}`;
+
+    const body = querystring.stringify({
+      grant_type: 'client_credentials',
+    });
     const {
-      data: { svctoken },
-    } = await axios.post(`${tokenUrl}?key=${apiKey}`, {
-      projectid,
-      serviceid,
-      secret,
+      data: { access_token },
+    } = await axios.post(`${tokenServiceUrl}/token`, body, {
+      headers: {
+        authorization: basicAuthToken,
+        'content-type': 'application/x-www-form-urlencoded',
+      },
     });
 
-    memoryCache.set(TOKEN_CACHE_KEY, svctoken, TOKEN_TTL);
-    logger.info(`get token, "${svctoken}"`);
-    return svctoken;
+    memoryCache.set(TOKEN_CACHE_KEY, access_token, TOKEN_TTL);
+    logger.info(`get token, "${access_token}"`);
+    return access_token;
   } catch (err) {
     logger.error(`Failed to get token. ERR "${err.message}"`);
     throw new Error(`Failed to get token. ERR "${err.message}"`);
